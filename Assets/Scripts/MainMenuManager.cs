58db2c9133227ddbb2d01a4152c57e1cf121e3d1
Bakamura <ra00297288@pucsp.edu.br>
2022-02-08 17:19:14 -0300

    Changes and additions to main menu
    Submenu buttons, constelation map redesigned

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public CanvasGroup mainMenu;
+    public CanvasGroup upgradeMenu;
+    public CanvasGroup constellationMenu;
+    public CanvasGroup settingsMenu;
+
+    public void OpenUpgradeMenu() {
+        HudManager.ActivateHudElement(upgradeMenu, true);
+        HudManager.ActivateHudElement(mainMenu, false);
+    }
+
+    public void OpenConstellationMenu() {
+        HudManager.ActivateHudElement(constellationMenu, true);
+        HudManager.ActivateHudElement(mainMenu, false);
+    }
+
+    public void OpenSettingsMenu() {
+        HudManager.ActivateHudElement(settingsMenu, true);
+        HudManager.ActivateHudElement(mainMenu, false);
+    }
+
+    public void CloseMenu() {
+        HudManager.ActivateHudElement(mainMenu, true);
+        HudManager.ActivateHudElement(upgradeMenu, false);
+        HudManager.ActivateHudElement(constellationMenu, false);
+        HudManager.ActivateHudElement(settingsMenu, false);
+    }
+
+}
